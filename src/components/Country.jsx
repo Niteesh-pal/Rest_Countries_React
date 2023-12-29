582d6978365a3ac13a13e7f2001b605fa92abf20
Niteesh-pal <niteeshpal801@gmail.com>
2023-12-29 20:47:32 +0530

    added subregion filter feature

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,78 +1,107 @@
-import "../index.css";
-import { useEffect,useState } from "react";
-import CountryData from "./CountryData";
-import BarLoader from "react-spinners/BarLoader";
+import '../index.css';
+import { useEffect, useState } from 'react';
+import CountryData from './CountryData';
+import BarLoader from 'react-spinners/BarLoader';
 
+function Country() {
+  let region = [];
+  let subregion = [];
+  const [apiData, setApiData] = useState([]);
+  const [userInput, setUserInput] = useState('');
+  const [value, setValue] = useState('Filter By Region');
+  const [active, setActive] = useState(false);
+  const [filterRegion, setFilteredRegion] = useState('Filter By Region');
+  const [isLoading, setIsLoading] = useState(false);
 
-function Country(){
-    let region = [];
-    const [apiData, setApiData]= useState([]);
-    const [userInput, setUserInput] = useState("");
-    const [active, setActive] = useState(false);
-    const [filterRegion, setFilteredRegion] = useState("Filter By Region");
-    const [isLoading, setIsLoading] = useState(false);
-  
-    useEffect(()=>{
-        fetch(`https://restcountries.com/v3.1/all`)
-        .then(data =>data.json())
-        .then((data)=>{setApiData(data);setIsLoading(true)})
-        .catch((error)=>{
-          console.error(error)
-        })
-        
-    },[]);
+  useEffect(() => {
+    fetch(`https://restcountries.com/v3.1/all`)
+      .then((data) => data.json())
+      .then((data) => {
+        setApiData(data);
+        setIsLoading(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    let filteredApiData = apiData.filter((country)=>{
-        const name = country.name.common.toLowerCase();
-        return (name.includes(userInput) && (filterRegion === country.region || filterRegion === "Filter By Region") )
-    }) 
+  let filteredApiData = apiData.filter((country) => {
+    const name = country.name.common.toLowerCase();
+    return (
+      name.includes(userInput) &&
+      (filterRegion === country.region ||
+        filterRegion === country.subregion ||
+        filterRegion === 'Filter By Region')
+    );
+  });
 
-    region = apiData.reduce((regionName, country)=>{
-              if(!regionName.includes(country.region)){
-                  regionName.push(country.region)
-              }
-              return regionName;
-         },[]).sort()
-       
-         
+  // console.log(filterRegion);
 
-            
-    return(
-        <>
-        <div className="filter-container container">
-            <div className="search">
-            <i className="fa-solid fa-magnifying-glass"></i>
-            <input type="text" placeholder="Search for a country..." onChange={(e)=>setUserInput(e.target.value.toLocaleLowerCase())}/>
-            </div>
-            <div className="filter">
-              <div className="filter-btn" onClick={()=>setActive(!active)}>
-                <span className="filter-btn-text">{filterRegion?filterRegion:"Filter By Region.."}</span>
-                <i className="fa-solid fa-angle-down"></i>
-              </div>
-              <div className={active?"filter-option active": "filter-option"}>
-                <ul onClick={(e)=>{setFilteredRegion(e.target.textContent), setActive(!active)}}>
-                  <li>Filter By Region</li>
-                  {
-                    region.map((regionName, index)=>{
-                      return <li key={index}>{regionName}</li>
-                    })
-                  }
-                </ul>
-              </div>
-            </div>
+  subregion = apiData.reduce((acc, curr) => {
+    if (!acc[curr.region]) {
+      acc[curr.region] = [];
+    } else {
+      if (!acc[curr.region].includes(curr.subregion)) {
+        acc[curr.region].push(curr.subregion);
+      }
+    }
+
+    return acc;
+  }, []);
+
+  return (
+    <>
+      <div className="filter-container container">
+        <div className="search">
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input
+            type="text"
+            placeholder="Search for a country..."
+            onChange={(e) => setUserInput(e.target.value.trim().toLowerCase())}
+          />
         </div>
-        <div className="country-container container">
-          {
-            isLoading === true? <CountryData filteredApiData={filteredApiData}/> : 
-            <div className="loader">
-            <BarLoader/>
-            </div>
-          }
+        <div className="filter">
+          <div className="filter-btn" onClick={() => setActive(!active)}>
+            <span className="filter-btn-text">
+              {filterRegion ? filterRegion : 'Filter By Region..'}
+            </span>
+            <i className="fa-solid fa-angle-down"></i>
+          </div>
+          <div className={active ? 'filter-option active' : 'filter-option'}>
+            <ul
+              className="filter-region"
+              onClick={(e) => {
+                setFilteredRegion(e.target.textContent);
+              }}
+            >
+              <li>Filter By Region</li>
+              {Object.keys(subregion).map((region, index) => {
+                return (
+                  <li key={index} className="region">
+                    <a>{region}</a>
+                    <ul onClick={(active) => setActive(!active)}>
+                      {subregion[region].map((name) =>
+                        name ? <li>{name}</li> : ''
+                      )}
+                    </ul>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-
-        </>
-    )
-
+      </div>
+      <div className="country-container container">
+        {isLoading === true ? (
+          <CountryData filteredApiData={filteredApiData} />
+        ) : (
+          <div className="loader">
+            <BarLoader />
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Country;
